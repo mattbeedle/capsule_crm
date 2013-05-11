bd70170aadbae47850edff54bf9730f444632c9d
Matt Beedle <mattbeedle@googlemail.com>
2013-05-12 00:23:00 +0200

    Basic cases stuff. Belongs to is going to need some more work

---
+++ lib/capsule_crm/case.rb
@@ -0,0 +1,258 @@
+module CapsuleCRM
+  class Case
+    include Virtus
+
+    extend  ActiveModel::Naming
+    include ActiveModel::Conversion
+    include ActiveModel::Validations
+
+    include CapsuleCRM::Associations::BelongsTo
+
+    attribute :id, Integer
+    attribute :name, String
+    attribute :description, String
+    attribute :status, String
+    attribute :close_date, Date
+    attribute :owner, String
+
+    validates :name, presence: true
+    validates :party, presence: true
+
+    belongs_to :party, class_name: 'CapsuleCRM::Party'
+
+    # Public: Search and retrieve all cases in CapsuleCRM
+    #
+    # options   - the Hash of query options (default: {}):
+    #             :tag            - the String tag to search for
+    #             :lastmodified   - the Date after which the case was last
+    #             modified by
+    #             :start          - the Integer index of the first record to
+    #             return.
+    #             :limit          - the Integer number of records to return
+    #
+    # Examples
+    #
+    # CapsuleCRM::Case.all(tag: 'Some Tag')
+    #
+    # Returns a CapsuleCRM::ResultsProxy of CapsuleCRM::Case objects
+    def self.all(options = {})
+      init_collection(
+        CapsuleCRM::Connection.get('/api/kase', options)['kases']['kase']
+      )
+    end
+
+    # Public: Search for a single case by ID
+    #
+    # id - the Integer ID of the record to search for
+    #
+    # Examples
+    #
+    # CapsuleCRM::Case.find(1)
+    #
+    # Returns a CapsuleCRM::Case object
+    def self.find(id)
+      new CapsuleCRM::Connection.get("/api/kase/#{id}")['kase']
+    end
+
+    # Public: Create a CapsuleCRM::Case
+    #
+    # attributes  - the Hash of attributes (default: {}):
+    #               :name         - the String name of the case (required)
+    #               :description  - the String description
+    #               :status       - the String status (OPEN or CLOSED), default
+    #               is OPEN
+    #               :owner        - the String username of the owner
+    #               :close_date   - the Date when the case was/will be closed.
+    #               Ignored if the status is set to CLOSED
+    #
+    # Examples
+    #
+    # CapsuleCRM::Case.create name: "Matt's test case"
+    #
+    # Returns a CapsuleCRM::Case
+    def self.create(attributes = {})
+      new(attributes).tap(&:save)
+    end
+
+    # Public: Create a CapsuleCRM::Case or raise a error
+    #
+    # attributes  - the Hash of attributes (default: {}):
+    #               :name         - the String name of the case (required)
+    #               :description  - the String description
+    #               :status       - the String status (OPEN or CLOSED), default
+    #               is OPEN
+    #               :owner        - the String username of the owner
+    #               :close_date   - the Date when the case was/will be closed.
+    #               Ignored if the status is set to CLOSED
+    #
+    # Examples
+    #
+    # CapsuleCRM::Case.create! name: "Matt's test case"
+    # => CapsuleCRM::Case object
+    #
+    # CapsuleCRM::Case.create! name: nil
+    # => CapsuleCRM::Errors::RecordInvalid
+    #
+    # Returns a CapsuleCRM::Case
+    def self.create!(attributes = {})
+      new(attributes).tap(&:save!)
+    end
+
+    # Public: Updates an existing CapsuleCRM::Case with the supplied attributes
+    #
+    # attributes  - the Hash of attributes (default: {}):
+    #               :name         - the String name of the case (required)
+    #               :description  - the String description
+    #               :status       - the String status (OPEN or CLOSED), default
+    #               is OPEN
+    #               :owner        - the String username of the owner
+    #               :close_date   - the Date when the case was/will be closed.
+    #               Ignored if the status is set to CLOSED
+    #
+    # Examples
+    #
+    # kase = CapsuleCRM::Case.find(1)
+    # kase.update_attributes name: 'A new name'
+    #
+    # Returns a CapsuleCRM::Case
+    def update_attributes(attributes = {})
+      self.attributes = attributes
+      save
+    end
+
+    # Public: Updates an existing CapsuleCRM::Case with the supplied attributes
+    # or raises an error
+    #
+    # attributes  - the Hash of attributes (default: {}):
+    #               :name         - the String name of the case (required)
+    #               :description  - the String description
+    #               :status       - the String status (OPEN or CLOSED), default
+    #               is OPEN
+    #               :owner        - the String username of the owner
+    #               :close_date   - the Date when the case was/will be closed.
+    #               Ignored if the status is set to CLOSED
+    #
+    # Examples
+    #
+    # kase = CapsuleCRM::Case.find(1)
+    # kase.update_attributes! name: 'A new name'
+    # => CapsuleCRM::Case object
+    #
+    # kase.update_attributes! name: nil
+    # => CapsuleCRM::Errors::RecordInvalid
+    #
+    # Returns a CapsuleCRM::Case
+    def update_attributes!(attributes = {})
+      self.attributes = attributes
+      save!
+    end
+
+    # Public: Saves this CapsuleCRM::Case
+    #
+    # Examples:
+    #
+    # kase = CapsuleCRM::Case.new(
+    #   name: 'Case Name', party: person_or_organization
+    # )
+    # kase.save
+    # => CapsuleCRM::Case object
+    #
+    # Returns the CapsuleCRM::Case or false
+    def save
+      if valid?
+        new_record? ? create_record : update_record
+      else
+        false
+      end
+    end
+
+    # Public: Saves this CapsuleCRM::Case or raises an error
+    #
+    # Examples
+    #
+    # kase = CapsuleCRM::Case.find(1)
+    # kase.name = 'Changed name'
+    # kase.save!
+    # => CapsuleCRM::Case object
+    #
+    # kase.name = nil
+    # kase.save!
+    # => CapsuleCRM::Errors::RecordInvalid
+    #
+    # Returns a CapsuleCRM::Case
+    def save!
+      if valid?
+        new_record? ? create_record : update_record
+      else
+        raise CapsuleCRM::Errors::RecordInvalid.new(self)
+      end
+    end
+
+    # Public: Deletes this CapsuleCRM::Case from CapsuleCRM
+    #
+    # Examples
+    #
+    # CapsuleCRM::Case.find(1).destroy
+    #
+    # Returns the CapsuleCRM::Case object
+    def destroy
+      self.id = nil if CapsuleCRM::Connection.delete("/api/kase/#{id}")
+      self
+    end
+
+    # Public: Is this case a new record?
+    #
+    # Examples
+    #
+    # CapsuleCRM::Case.find(1).new_record?
+    # => false
+    #
+    # CapsuleCRM::Case.new.new_record?
+    # => true
+    #
+    # Returns a Boolean
+    def new_record?
+      !id
+    end
+
+    # Public: Is this case persisted to CapsuleCRM?
+    #
+    # Examples
+    #
+    # CapsuleCRM::Case.find(1).persisted?
+    # => true
+    #
+    # CapsuleCRM::Case.new.persisted?
+    # => false
+    #
+    # Returns a Boolean
+    def persisted?
+      !new_record?
+    end
+
+    def to_capsule_json
+      {
+        kase: CapsuleCRM::HashHelper.camelize_keys(
+          attributes.dup.delete_if { |key, value| value.blank? }
+        )
+      }
+    end
+
+    private
+
+    def create_record
+      self.attributes = CapsuleCRM::Connection.post(
+        "/api/party/#{party_id}/kase", to_capsule_json
+      )
+      self
+    end
+
+    def update_record
+      CapsuleCRM::Connection.put("/api/kase/#{id}", to_capsule_json)
+    end
+
+    def self.init_collection(results)
+      CapsuleCRM::ResultsProxy.new(results.map { |item| new item })
+    end
+  end
+end
---
+++ spec/lib/capsule_crm/case_spec.rb
@@ -0,0 +1,317 @@
+require 'spec_helper'
+
+describe CapsuleCRM::Case do
+  before { configure }
+
+  it { should validate_presence_of(:name) }
+
+  it { should validate_presence_of(:party) }
+
+  describe '.all' do
+    before do
+      stub_request(:get, 'https://1234:@company.capsulecrm.com/api/kase').
+        to_return(body: File.read('spec/support/all_cases.json'))
+    end
+
+    subject { CapsuleCRM::Case.all }
+
+    it { should be_a(Array) }
+
+    it { subject.length.should eql(1) }
+
+    it { subject.all? { |item| item.is_a?(CapsuleCRM::Case) }.should be_true }
+  end
+
+  describe '.find' do
+    before do
+      stub_request(:get, 'https://1234:@company.capsulecrm.com/api/kase/43').
+        to_return(body: File.read('spec/support/case.json'))
+    end
+
+    subject { CapsuleCRM::Case.find(43) }
+
+    it { should be_a(CapsuleCRM::Case) }
+
+    it { subject.name.should eql('Consulting') }
+  end
+
+  describe '.create' do
+    context 'when the case is valid' do
+      before do
+        stub_request(
+          :post, 'https://1234:@company.capsulecrm.com/api/party/1/kase'
+        ).to_return(
+          headers: { 'Location' => 'https://sample.capsulecrm.com/api/kase/59' }
+        )
+      end
+
+      let(:party) { CapsuleCRM::Person.new(id: 1, first_name: 'Matt') }
+
+      subject { CapsuleCRM::Case.create name: 'Test Case', party: party }
+
+      it { should be_a(CapsuleCRM::Case) }
+
+      it { subject.id.should eql(59) }
+    end
+
+    context 'when the case is invalid' do
+      let(:kase) { CapsuleCRM::Case.create name: 'Test Case' }
+
+      it { kase.errors.should_not be_blank }
+    end
+  end
+
+  describe '.create!' do
+    context 'when the case is valid' do
+      before do
+        stub_request(
+          :post, 'https://1234:@company.capsulecrm.com/api/party/1/kase'
+        ).to_return(
+          headers: { 'Location' => 'https://sample.capsulecrm.com/api/kase/61' }
+        )
+      end
+
+      let(:party) { CapsuleCRM::Person.new(id: 1, first_name: 'Matt') }
+
+      subject { CapsuleCRM::Case.create! name: 'Test Case', party: party }
+
+      it { should be_a(CapsuleCRM::Case) }
+
+      it { subject.id.should eql(61) }
+    end
+
+    context 'when the case is invalid' do
+      let(:kase) { CapsuleCRM::Case.create! name: 'Test Case' }
+
+      it 'should raise an error' do
+        expect { kase }.to raise_error(CapsuleCRM::Errors::RecordInvalid)
+      end
+    end
+  end
+
+  describe '#update_attributes' do
+    context 'when the case is valid' do
+      let(:party) { CapsuleCRM::Person.new(id: 10) }
+
+      let(:kase) do
+        CapsuleCRM::Case.new(id: 1, name: 'Test Case', party: party)
+      end
+
+      before do
+        stub_request(
+          :put, 'https://1234:@company.capsulecrm.com/api/kase/1'
+        ).to_return(status: 200)
+        kase.update_attributes name: 'changed name'
+      end
+
+      it { kase.name.should eql('changed name') }
+
+      it { kase.id.should eql(1) }
+    end
+
+    context 'when the case is not valid' do
+      let(:kase) { CapsuleCRM::Case.new(id: 2, name: 'Test Case') }
+
+      before do
+        kase.update_attributes name: 'changed name'
+      end
+
+      it { kase.errors.should_not be_blank }
+
+      it { kase.name.should eql('changed name') }
+    end
+  end
+
+  describe '#update_attributes!' do
+    context 'when the case is valid' do
+      let(:party) { CapsuleCRM::Person.new(id: 1, first_name: 'Matt') }
+
+      let(:kase) do
+        CapsuleCRM::Case.new(id: 1, name: 'Test Case', party: party)
+      end
+
+      before do
+        stub_request(
+          :put, 'https://1234:@company.capsulecrm.com/api/kase/1'
+        ).to_return(status: 200)
+        kase.update_attributes! name: 'changed name'
+      end
+
+      it { kase.name.should eql('changed name') }
+
+      it { kase.id.should eql(1) }
+    end
+
+    context 'when the case is invalid' do
+      let(:kase) { CapsuleCRM::Case.new(id: 1, name: 'Test Case') }
+
+      subject { kase.update_attributes! name: 'changed name' }
+
+      it do
+        expect { subject }.to raise_error(CapsuleCRM::Errors::RecordInvalid)
+      end
+    end
+  end
+
+  describe '#save' do
+    let(:party) { CapsuleCRM::Person.new(id: 1) }
+
+    context 'when it is a new record' do
+      context 'when it is valid' do
+        let(:kase) { CapsuleCRM::Case.new(name: 'Test', party: party) }
+
+        before do
+          stub_request(
+            :post, 'https://1234:@company.capsulecrm.com/api/party/1/kase'
+          ).to_return(
+            headers: { 'Location' => 'https://sample.capsulecrm.com/api/kase/2' }
+          )
+          kase.save
+        end
+
+        subject { kase }
+
+        it { should be_persisted }
+
+        it { kase.id.should eql(2) }
+      end
+
+      context 'when it is invalid' do
+        let(:kase) { CapsuleCRM::Case.new(name: 'Test') }
+
+        before { kase.save }
+
+        it { kase.errors.should_not be_blank }
+
+        it { kase.should_not be_persisted }
+      end
+    end
+
+    context 'when it is an existing record' do
+      let(:party) { CapsuleCRM::Person.new(id: 1) }
+
+      context 'when it is valid' do
+        let(:kase) { CapsuleCRM::Case.new(id: 2, name: 'Test', party: party) }
+
+        before do
+          stub_request(
+            :put, 'https://1234:@company.capsulecrm.com/api/kase/2'
+          ).to_return(status: 200)
+          kase.name = 'changed name'
+          kase.save
+        end
+
+        it { kase.should be_persisted }
+
+        it { kase.name.should eql('changed name') }
+      end
+
+      context 'when it is invalid' do
+        let(:kase) { CapsuleCRM::Case.new(id: 2, name: 'Test') }
+
+        before { kase.save }
+
+        it { kase.should be_persisted }
+
+        it { kase.errors.should_not be_blank }
+      end
+    end
+  end
+
+  describe '#save!' do
+    let(:party) { CapsuleCRM::Person.new(id: 1) }
+
+    context 'when it is a new record' do
+      context 'when it is valid' do
+        let(:kase) { CapsuleCRM::Case.new(name: 'Test Case', party: party) }
+
+        before do
+          stub_request(:post, /^https.*\/api\/party\/1\/kase$/).
+            to_return(headers: {
+            'Location' => 'https://sample.capsulecrm.com/api/kase/59'
+          })
+          kase.save!
+        end
+
+        it { kase.should be_persisted }
+      end
+
+      context 'when it is invalid' do
+        let(:kase) { CapsuleCRM::Case.new(name: 'Test Case') }
+
+        it do
+          expect { kase.save! }.
+            to raise_error(CapsuleCRM::Errors::RecordInvalid)
+        end
+      end
+    end
+
+    context 'when it is not a new record' do
+      context 'when it is valid' do
+        let(:kase) do
+          CapsuleCRM::Case.new(id: 1, name: 'Test Case', party: party)
+        end
+
+        before do
+          stub_request(:put, /^.*\/api\/kase\/1$/).to_return(status: 200)
+          kase.save!
+        end
+
+        it { kase.should be_persisted }
+      end
+
+      context 'when it is invalid' do
+        let(:kase) do
+          CapsuleCRM::Case.new(id: 1, name: 'Test Case')
+        end
+
+        it do
+          expect { kase.save! }.
+            to raise_error(CapsuleCRM::Errors::RecordInvalid)
+        end
+      end
+    end
+  end
+
+  describe '#destroy' do
+    let(:party) { CapsuleCRM::Person.new(id: 2) }
+
+    let(:kase) { CapsuleCRM::Case.new(id: 1, party: party, name: 'Test Case') }
+
+    before do
+      stub_request(:delete, /^.*\/api\/kase\/1$/).to_return(status: 200)
+      kase.destroy
+    end
+
+    it { kase.should_not be_persisted }
+  end
+
+  describe '#new_record?' do
+    describe 'when it is a new record' do
+
+      subject { CapsuleCRM::Case.new }
+
+      it { should be_new_record }
+    end
+
+    describe 'when it is not a new record' do
+      subject { CapsuleCRM::Case.new(id: 1) }
+
+      it { should_not be_new_record }
+    end
+  end
+
+  describe '#persisted?' do
+    context 'when it is persisted' do
+      subject { CapsuleCRM::Case.new(id: 1) }
+
+      it { should be_persisted }
+    end
+
+    context 'when it is not persisted' do
+      subject { CapsuleCRM::Case.new }
+
+      it { should_not be_persisted }
+    end
+  end
+end
