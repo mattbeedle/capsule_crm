fad9205b3d0c85c5048ea28ecd88f413417b6bb7
Matt Beedle <mattbeedle@googlemail.com>
2013-05-12 13:31:06 +0200

    Add Users

---
+++ lib/capsule_crm/user.rb
@@ -0,0 +1,34 @@
+module CapsuleCRM
+  class User
+    include Virtus
+
+    extend  ActiveModel::Naming
+    include ActiveModel::Conversion
+    include ActiveModel::Validations
+
+    include CapsuleCRM::Associations::BelongsTo
+
+    attribute :username, String
+    attribute :name, String
+    attribute :currency, String
+    attribute :timezone, String
+    attribute :logged_in, Boolean
+
+    belongs_to :party, class_name: 'CapsuleCRM::Party'
+
+    # Public: Retrieve all users from CapsuleCRM
+    #
+    # Examples:
+    #
+    # CapsuleCRM::User.all
+    #
+    # Returns a CapsuleCRM::ResultsProxy of CapsuleCRM::User objects
+    def self.all
+      CapsuleCRM::ResultsProxy.new(
+        CapsuleCRM::Connection.get('/api/users')['users']['user'].map do |item|
+          new item
+        end
+      )
+    end
+  end
+end
