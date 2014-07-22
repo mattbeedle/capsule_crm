1208dfa9c614c07a24590c11a089ef72e3417f00
Matt Beedle <mattbeedle@googlemail.com>
2014-07-22 21:07:30 +0200

    Refactor connection a little bit

--- lib/capsule_crm/connection.rb
+++ lib/capsule_crm/connection.rb
@@ -1,54 +1,66 @@
 module CapsuleCRM
   class Connection
-
-    # Public: Send a GET request to CapsuleCRM API
-    #
-    # path    - The String path where the request should go
-    # params  - The Hash of URL parameters
-    #
-    # Returns a Hash from the JSON response
     def self.get(path, params = {})
+      new.get(path, params)
+    end
+
+    def get(path, params)
       preprocess_params(params)
       JSON.parse request(:get, path, params).body
     end
 
     def self.post(path, params = {})
+      new.post(path, params)
+    end
+
+    def post(path, params)
       process_post_response request(:post, path, params.to_json)
     end
 
-    def self.put(path, params)
+    def self.put(path, params = {})
+      new.put(path, params)
+    end
+
+    def put(path, params)
       request(:put, path, params.to_json).success?
     end
 
     def self.delete(path)
-      request(:delete, path, {}).success?
+      new.delete(path)
+    end
+
+    def delete(path)
+      request(:delete, path).success?
     end
 
     private
 
-    def self.request(method, path, params)
-      CapsuleCRM.log "CapsuleCRM: #{method.upcase} #{path} with #{params}"
+    def request(method, path, params = {})
+      log "CapsuleCRM: #{method.upcase} #{path} with #{params}"
       faraday.send(method, path, params) do |req|
         req.headers.update default_request_headers
       end.tap do |response|
-        CapsuleCRM.log "CapsuleCRM Response: #{response.body}"
+        log "CapsuleCRM Response: #{response.body}"
       end
     end
 
-    def self.preprocess_params(params)
+    def log(string)
+      CapsuleCRM.log string
+    end
+
+    def preprocess_params(params)
       params.symbolize_keys!
       if params_contains_lastmodified(params)
         params[:lastmodified] = params[:lastmodified].strftime("%Y%m%dT%H%M%S")
       end
     end
 
-    def self.params_contains_lastmodified(params)
+    def params_contains_lastmodified(params)
       params.keys.include?(:lastmodified) &&
         params[:lastmodified].respond_to?(:strftime)
     end
 
-    # TODO clean this shit up
-    def self.process_post_response(response)
+    def process_post_response(response)
       if response.headers['Location'] &&
         match = response.headers['Location'].match(/\/(?<id>\d+)$/)
         { id: match[:id].to_i }
@@ -57,20 +69,24 @@ module CapsuleCRM
       end
     end
 
-    def self.default_request_headers
+    def default_request_headers
       { accept: 'application/json', content_type: 'application/json' }
     end
 
-    def self.faraday
-      ::Faraday.new("https://#{subdomain}.capsulecrm.com").tap do |connection|
+    def faraday
+      @faraday ||= ::Faraday.new("https://#{host}").tap do |connection|
         connection.basic_auth(CapsuleCRM.configuration.api_token, '')
         connection.request  :json
         connection.use CapsuleCRM::Faraday::Middleware::RaiseError
       end
     end
 
-    def self.subdomain
-      CapsuleCRM.configuration.subdomain
+    def host
+      @host ||= "#{subdomain}.capsulecrm.com"
+    end
+
+    def subdomain
+      @subdomain ||= CapsuleCRM.configuration.subdomain
     end
   end
 end
