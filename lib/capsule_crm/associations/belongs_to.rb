4b5f5118955b2aa719af8d73e384cd6ee6760ca8
Matt Beedle <mattbeedle@googlemail.com>
2014-09-10 10:31:35 +0200

    Cleanup belongs_to.rb according to Rubocop rules

--- lib/capsule_crm/associations/belongs_to.rb
+++ lib/capsule_crm/associations/belongs_to.rb
@@ -2,12 +2,12 @@ require_relative 'belongs_to_association'
 require_relative 'belongs_to_finder'
 
 module CapsuleCRM
-  module Associations
-    module BelongsTo
+  module Associations # nodoc
+    module BelongsTo # nodoc
       extend ActiveSupport::Concern
 
+      # nodoc
       module ClassMethods
-
         # Public: Add getter and setter methods for belongs to associations
         #
         # association_name  - The String name of the association
@@ -32,9 +32,9 @@ module CapsuleCRM
         # person.organisation
         # => organisation
         def belongs_to(association_name, options = {})
-          association = CapsuleCRM::Associations::BelongsToAssociation.
-            new(association_name, self, options)
-          self.associations[association_name] = association
+          association = CapsuleCRM::Associations::BelongsToAssociation
+            .new(association_name, self, options)
+          associations[association_name] = association
 
           class_eval do
             attribute association.foreign_key, Integer
@@ -42,24 +42,24 @@ module CapsuleCRM
 
           (class << self; self; end).instance_eval do
             define_method "_for_#{association_name}" do |id|
-              CapsuleCRM::Associations::BelongsToFinder.new(association).
-                call(id)
+              CapsuleCRM::Associations::BelongsToFinder.new(association)
+                .call(id)
             end
           end
 
           define_method association_name do
             instance_variable_get(:"@#{association_name}") ||
-              if self.send(association.foreign_key)
+              if send(association.foreign_key)
                 association.parent(self).tap do |object|
-                  self.send("#{association_name}=", object)
+                  send("#{association_name}=", object)
                 end
               end
           end
 
           define_method "#{association_name}=" do |associated_object|
-            associated_object.tap do |object|
+            associated_object.tap do
               instance_variable_set(:"@#{association_name}", associated_object)
-              self.send "#{association.foreign_key}=", associated_object.try(:id)
+              send "#{association.foreign_key}=", associated_object.try(:id)
             end
           end
         end
